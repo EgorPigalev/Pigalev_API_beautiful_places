401b10869b1947053edca27cf9f98a4dad6f7e29
Егор Пигалёв <1pigaleve@mail.ru>
2022-12-08 16:00:41 +0300

    Доработка

---
+++ Pigalev_API_beautiful_places/Models/ImagesModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pigalev_API_beautiful_places.Models
+{
+    public class ImagesModel
+    {
+    }
+}
---
+++ Pigalev_API_beautiful_places/Models/UsersModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pigalev_API_beautiful_places.Models
+{
+    public class UsersModel
+    {
+
+    }
+}
